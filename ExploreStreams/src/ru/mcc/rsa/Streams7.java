ac5cc5ee43bafca71dbe564326e7d7707ef3ba13
maa <maa@PC14-2467.mcc.rsa.ru>
2017-03-24 17:28:55 +0300

    Another collector demo has been added.

--- ExploreStreams/src/ru/mcc/rsa/Streams7.java
+++ ExploreStreams/src/ru/mcc/rsa/Streams7.java
@@ -9,26 +9,37 @@ import ru.rsa.mcc.DemoData;
 import ru.rsa.mcc.Invoice;
 
 public class Streams7 {
-
 	public static void main(String[] args) {
 		List<Invoice> invoices = DemoData.populateDemoData();
 		DemoData.printDemoData(invoices);
 		/*
-		 * It's also possible to group and reduce stream's input element
-		 * Let's calculate a invoices summary amount by a customer
+		 * The operations you have seen so far were either returning another
+		 * stream (i.e., intermediate operations) or returning a value, such as a
+		 * Double, or an  Optional object (i.e., terminal operations).
+		 * By contrast, the  collect method is a terminal operation. It lets you
+		 * accumulate the elements of a stream into a summary result.
+		 * The argument passed to  collect is an object of type
+		 * java.util.stream.Collector. A  Collector  object essentially
+		 * describes a recipe for accumulating the elements of a stream into a
+		 * final result. The factory method  Collectors.toList()used earlier
+		 * returns a  Collector object describing how to accumulate a stream
+		 * into a  List. However, there are many similar built-in collectors
+		 * available, which you can see in the class  Collectors. For example,
+		 * you can group invoices by customers using Collectors.groupingBy
+		 * as shown here
 		 */
-		Map<String, Double> totalAmountByCustomer = invoices.stream()
-															.collect(Collectors.groupingBy(
-																	 Invoice::getCustomerName,
-																	 Collectors.reducing(
-																			 0.0,
-																			 Invoice::getAmount,
-																			 Double::sum)));
+		Map<String, List<Invoice>> customerToInvoices = invoices.stream()
+																.collect(
+																	Collectors.groupingBy(Invoice::getCustomerName)
+																);
 		
-		List<Map.Entry<String, Double>> totalAmountByCustomerList = new ArrayList<>(totalAmountByCustomer.entrySet());
+		// print this information out
+		List<Map.Entry<String, List<Invoice>>> totalAmountByCustomerList = new ArrayList<>(customerToInvoices.entrySet());
 		
 		totalAmountByCustomerList.stream()
-								 .forEach(g -> System.out.printf("Customer %s, total amount %f\n", g.getKey(), g.getValue()));
-				
+		 						 .forEach(g -> { 
+		 							 System.out.printf("Orders for customer %s:\n", g.getKey());
+		 							 DemoData.printDemoData(g.getValue());
+		 						 });
 	}
 }
---
+++ ExploreStreams/src/ru/mcc/rsa/Streams8.java
@@ -0,0 +1,35 @@
+package ru.mcc.rsa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import ru.rsa.mcc.DemoData;
+import ru.rsa.mcc.Invoice;
+
+public class Streams8 {
+		public static void main(String[] args) {
+			List<Invoice> invoices = DemoData.populateDemoData();
+			DemoData.printDemoData(invoices);
+			/*
+			 * More complex example or reducing and collecting
+			 * It's also possible to group and reduce stream's input element
+			 * Let's calculate a invoices summary amount by a customer
+			 */
+			Map<String, Double> totalAmountByCustomer = invoices.stream()
+																.collect(Collectors.groupingBy(
+																		 Invoice::getCustomerName,
+																		 Collectors.reducing(
+																				 0.0,
+																				 Invoice::getAmount,
+																				 Double::sum)));
+			
+			List<Map.Entry<String, Double>> totalAmountByCustomerList = new ArrayList<>(totalAmountByCustomer.entrySet());
+			
+			totalAmountByCustomerList.stream()
+									 .forEach(g -> System.out.printf("Customer %s, total amount %f\n", g.getKey(), g.getValue()));
+					
+		}
+				
+}
